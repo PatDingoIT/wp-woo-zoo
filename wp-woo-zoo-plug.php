4513eac07e599467f31a7db79961a2139574ced8
PatDingoIT <patrick@dingoit.com.au>
2015-01-21 08:59:18 +0800

    Create wp-woo-zoo-plug.php

---
+++ wp-woo-zoo-plug.php
@@ -0,0 +1,10 @@
+<?php // Opening PHP tag
+/**
+* Plugin Name: WP Woo-Zoolz Integration
+* Plugin URI: http://mypluginuri.com/
+* Description: Integrating Zoolz for Resellers with Wordpress and Woo Commerce.
+* Version: 1.0
+* Author: Patrick D Thomas
+* Author URI: www.dingoIT.com.au
+* License: MIT License
+*/
